3d21621999f92b2a0bbb110b8f406790fdfa642d
XintongTheCoder <xintongmi@gmail.com>
2023-05-28 15:00:26 -0700

    build integration test for GET /products

--- server/test/integrationTest.test.js
+++ server/test/integrationTest.test.js
@@ -4,9 +4,11 @@ const dbConfig = require('../config/db.config');
 const server = require('../index');
 
 describe('Server integration test', () => {
+  let subscription;
   let pool;
 
-  beforeAll(() => {
+  beforeEach(() => {
+    subscription = server.listen(8082);
     pool = new Pool({
       host: dbConfig.HOST,
       user: dbConfig.USER,
@@ -17,32 +19,46 @@ describe('Server integration test', () => {
   });
 
   afterEach(() => {
-    jest.clearAllMocks();
+    pool.end();
+    subscription.close();
   });
 
-  it('GET /products', async () => {
+  it('GET /products should have correct route and response with correct data', (done) => {
     supertest(server)
       .get('/products')
       .expect(200)
       .then((response) => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length).toBe(5);
+        const products = JSON.parse(response.text);
+        expect(products.length).toBe(5);
+        done();
+      });
+  });
+
+  it.only('GET /products should handle pagination correctly', (done) => {
+    supertest(server)
+      .get('/products')
+      .query({ page: 1, count: 2 })
+      .expect(200)
+      .then((response) => {
+        const products = JSON.parse(response.text);
+        expect(products.length).toBe(2);
+        // expect(products[0].id).toBe(485620);
+        done();
+      });
+  });
+
+  it('GET /products/:product_id should have correct route and response with correct data', (done) => {
+    const productID = '485620';
+    supertest(server)
+      .get(`/products/${productID}`)
+      .then((response) => {
+        const product = JSON.parse(response.text);
+        expect(response.status).toBe(200);
+        expect(product.id).toBe(485620);
+        done();
       });
   });
 });
-
-// it('GET /products/:product_id should have correct route and response with correct data', (done) => {
-//   pool.query = () => Promise.resolve({ rows: [mockProducts[0]] });
-//   const product_id = '485620';
-//   supertest(server)
-//     .get(`/products/${product_id}`)
-//     .then((response) => {
-//       const product = JSON.parse(response.text);
-//       expect(response.status).toBe(200);
-//       expect(product.id).toBe(485620);
-//       done();
-//     });
-// });
 
 // it('GET /products/:product_id/styles should have correct route and response with correct data', (done) => {
 //   pool.query = () => Promise.resolve({ rows: mockStyles });
