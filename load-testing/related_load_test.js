a178ebcd410713c7a302a59279efb1e86d22cd00
XintongTheCoder <xintongmi@gmail.com>
2023-05-30 12:41:04 -0700

    add load-testing scripts for product, style and related

---
+++ load-testing/related_load_test.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 1000 },
+    { duration: '30s', target: 1000 },
+    { duration: '10s', target: 0 },
+  ],
+};
+
+export default () => {
+  const productID = Math.floor(Math.random() * 1000000);
+  const res = http.get(http.url`http://localhost:8080/api/products/${productID}/related`);
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+  sleep(1);
+};
